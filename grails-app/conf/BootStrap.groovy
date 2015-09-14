7cec50c5ad325f0cbe258b9a91632d4b4c608ff4
xp <13910818313@163.com>
2015-09-14 13:55:51 +0800

    init

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,7 @@
 import com.bjrxht.converters.marshaller.json.TableDomainClassMarshaller
+import com.bjrxht.declare.Declare
 import grails.converters.JSON
+import org.bouncycastle.jce.provider.symmetric.Grain128
 import org.codehaus.groovy.grails.plugins.springsecurity.SecurityFilterPosition
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 import com.bjrxht.core.*
@@ -11,20 +13,24 @@ class BootStrap {
     def init = { servletContext ->
         SpringSecurityUtils.clientRegisterFilter('concurrencyFilter', SecurityFilterPosition.CONCURRENT_SESSION_FILTER)
         JSON.registerObjectMarshaller(new TableDomainClassMarshaller(false, true, 10));
+        if(BaseUser.count()==0){
+            createRole();
+            createDefaultUsers();
+            createRequestMap();
+            creatDeclares();
+            createUsers();
+        }
     }
     def destroy = {
     }
 
-    private void creatDeclares(){
-
-    }
     private createRole(){
         def roles=[
                 ['IS_AUTHENTICATED_ANONYMOUSLY','匿名用户'],
                 ['IS_AUTHENTICATED_FULLY','登录用户'],
                 ['IS_AUTHENTICATED_REMEMBERED','缓存用户'],
-                ['ROLE_SUPERADMIN','系统管理员'],
-                ['ROLE_ADMIN','申报事项管理员'],
+                ['ROLE_ADMIN','系统管理员'],
+                ['ROLE_MANAGER','申报事项管理员'],
                 ['ROLE_USER','申报用户'] ,
                 ['ROLE_EXPERT','初评专家'],
                 ['ROLE_AUDIT_EXPERT','评审专家']
@@ -38,18 +44,11 @@ class BootStrap {
         }
     }
     def createDefaultUsers() {
-        def superadmin = BaseUser.findByUsername('superadmin')
-        if (!superadmin) {
-            superadmin = new BaseUser(username:'superadmin',password: 'superadmin',enabled:true,accountExpired:false,accountLocked:false,passwordExpired:false)
-            superadmin.save(flush: true)
-        }
         def admin = BaseUser.findByUsername('admin')
         if (!admin) {
-            admin = new BaseUser(username:'admin',password: 'admin',enabled:true,accountExpired:false,accountLocked:false,passwordExpired:false)
+            admin = new BaseUser(email: 'admin@bjrxht.com',firstName: '',lastName: 'admin',username:'admin',password: 'admin',enabled:true,accountExpired:false,accountLocked:false,passwordExpired:false)
             admin.save(flush: true)
         }
-
-        new BaseUserBaseRole(baseUser:superadmin,baseRole:BaseRole.findByAuthority('ROLE_SUPERADMIN')).save(flush: true);
         new BaseUserBaseRole(baseUser:admin,baseRole:BaseRole.findByAuthority('ROLE_ADMIN')).save(flush: true);
 
     }
@@ -67,12 +66,21 @@ class BootStrap {
         new Requestmap(url: '/register/**', configAttribute: 'IS_AUTHENTICATED_ANONYMOUSLY').save(flush: true)
         new Requestmap(url: '/j_spring_security_check', configAttribute: 'IS_AUTHENTICATED_ANONYMOUSLY').save(flush: true)
         new Requestmap(url: '/documents/**', configAttribute: 'IS_AUTHENTICATED_ANONYMOUSLY').save(flush: true)
-        new Requestmap(url: '/baseUser/**', configAttribute: 'ROLE_SUPERADMIN').save(flush: true)
+        new Requestmap(url: '/baseUser/**', configAttribute: 'ROLE_ADMIN').save(flush: true)
         new Requestmap(url: '/expert/**', configAttribute: 'ROLE_EXPERT,ROLE_AUDIT_EXPERT').save(flush: true)
         new Requestmap(url: '/pdf/**', configAttribute: 'IS_AUTHENTICATED_FULLY').save(flush: true)
         new Requestmap(url: '/', configAttribute: 'IS_AUTHENTICATED_ANONYMOUSLY').save(flush: true)
         new Requestmap(url: '/uploads/**', configAttribute: 'IS_AUTHENTICATED_ANONYMOUSLY').save(flush: true)
+
         new Requestmap(url: '/**', configAttribute: 'IS_AUTHENTICATED_FULLY').save(flush: true)
+
+    }
+
+    private void creatDeclares(){
+        def declare=new Declare(name:'申报示例',code:'declare1',domainName: 'localhost',regBeginDate: new Date(),regEndDate: new Date()+30,applyBeginDate: new Date(),applyEndDate: new Date()+60);
+        declare.save(flush: true)
+    }
+    private void createUsers(){
 
     }
 }
